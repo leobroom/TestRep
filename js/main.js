6ce745eed50f3d6ec9630fc2174dcab50d4359be
leobroom <leonbrohmann@gmx.de>
2018-09-05 22:14:23 +0200

    alles gut

--- js/main.js
+++ js/main.js
@@ -26,9 +26,18 @@ document.body.appendChild(renderer.domElement);
 // ------------------------------------------------
 
 // create a spot light
-var spotLight = new THREE.SpotLight(0xffffff, 1);
-spotLight.position.set(0, 0, 10);
-scene.add(spotLight);
+// var spotLight = new THREE.SpotLight(0xffffff, 1);
+// spotLight.position.set(0,10, 10);
+// scene.add(spotLight);
+var light1 = new THREE.DirectionalLight( 0xffffff, 0.5 );
+
+light1.position.set( 10, 10, 10 );
+scene.add( light1 );
+                
+// var light2 = new THREE.DirectionalLight( 0xffffff, 1.5 );
+
+// light2.position.set( 0, -10, 0 );
+// scene.add( light2 );
 
 
 // ------------------------------------------------
@@ -55,15 +64,15 @@ geom.receiveShadow = false; //default
 var material2 = new THREE.MeshPhongMaterial({ color: "#433F81" });
 material2.side = THREE.DoubleSide;
 
-var object = new THREE.Mesh(geom, material2);
+var objectBLA = new THREE.Mesh(geom, material2);
 
-//scene.add(object);
+scene.add(objectBLA);
 
 // ------------------------------------------------
 // SPHERE
 // ------------------------------------------------
 
-var spHgeometry = new THREE.SphereGeometry(0.04, 4, 4);
+var spHgeometry = new THREE.SphereGeometry(0.02, 4, 4);
 var yellow = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 var red = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 // var yellowPt = new THREE.Mesh( spHgeometry, yellow );
@@ -71,9 +80,39 @@ var red = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 // scene.add( yellowPt );
 // scene.add( redPt );
 
-
 // yellowPt.position.copy(v0);
 // redPt.position.copy(v2);
+
+// create a simple square shape. We duplicate the top left and bottom right
+// vertices because each vertex needs to appear once per triangle.
+var vertices = new Float32Array( [
+	-1.0, -1.0,  1.0,
+	 1.0, -1.0,  1.0,
+	 1.0,  1.0,  1.0,
+
+	 1.0,  1.0,  1.0,
+	-1.0,  1.0,  1.0,
+	-1.0, -1.0,  1.0
+] );
+
+
+
+
+// itemSize = 3 because there are 3 values (components) per vertex
+var blaaa = new THREE.BufferGeometry();
+blaaa.addAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+blaaa.addAttribute( 'normal', new THREE.BufferAttribute( vertices, 3 ) );
+//blaaa.addAttribute( 'color', new THREE.BufferAttribute( vertices, 3 ) );
+//var material = new THREE.MeshPhongMaterial( { color: 0xff0000 } );
+// var material = new THREE.MeshPhongMaterial( {
+//     color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
+//     side: THREE.DoubleSide, vertexColors: THREE.VertexColors
+// } );
+// var meshSquare = new THREE.Mesh( blaaa, material2 );
+
+
+
+//    scene.add( meshSquare );
 
 // ------------------------------------------------
 // Wireframe
@@ -103,22 +142,22 @@ scene.add(group);
 var nurbsDegree = 2;
 
 var nurbsControlPointsUp = [
-    new THREE.Vector4(-5, 2.5, 0, 1),
-    new THREE.Vector4(-2.5, 3, 0, 1),
+    new THREE.Vector4(-4, 2.5, 0, 1),
+    new THREE.Vector4(-2.5, 2.8, 0, 1),
     new THREE.Vector4(2.5, 2, 0, 1),
-    new THREE.Vector4(5, 2.5, 0, 1),
+    new THREE.Vector4(3, 2.5, 0, 1),
 ];
 
 var nurbsControlPointsDown = [
-    new THREE.Vector4(-5, -2.5, 0, 4),
-    new THREE.Vector4(-3, -3, 0, 1),
+    new THREE.Vector4(-3.4, -2.5, 0, 4),
+    new THREE.Vector4(-3, -2.3, 0, 1),
     new THREE.Vector4(2, -2, 0, 2),
-    new THREE.Vector4(5, -2.5, 0, 1),
+    new THREE.Vector4(3.6, -2.1, 0, 1),
 ];
 
-var pointValue = 20;
-var uDivision = 40;
-var vDivision = 300;
+var pointValue = 50;
+var uDivision = 2;
+var vDivision = 3;
 var nurbsKnots = NURBSHelper.getNurbsKnots(nurbsControlPointsUp, nurbsDegree);
 var nurbsMaterial = new THREE.LineBasicMaterial({ linewidth: 0.15, color: 0xFFFFFF });
 
@@ -241,8 +280,6 @@ for (v = 1; v < vDivision + 1; v += 2) {
     }
 }
 
-
-
 rowA.forEach(cp => {
     var pt = new THREE.Mesh(spHgeometry, red);
     scene.add(pt);
@@ -259,12 +296,140 @@ rowB.forEach(cp => {
 
 
 
+//-------------------------------------------
+//TriangleCount
+//-------------------------------------------
 
+var triangleEachRow = 2 * uDivision + 1;
+var triangleCount = triangleEachRow * (vDivision-1);
 
+console.log("triangleEachRow: " + triangleEachRow);
+console.log("triangleCount: " + triangleCount);
 
+console.log("ALL Size: " + triangleCount*3);
 
+// 9 ,da 3 float * 3 Vec
+var all = new Float32Array(triangleCount*3);
 
+all.forEach(numb => {
+numb =0.0;
+});
 
+// End
+
+//-------------------------------------------
+//Triangle Up A
+//-------------------------------------------
+
+//Anzahl der Reihen
+for (v = 0; v < countVOdd; v++) {
+    var rowAIndex = v * aCount;
+    var rowBIndex = v * bCount;
+
+    var idx = Triangle.constructIdx(v, triangleEachRow, 1);
+
+    console.log("idx: " + idx);
+
+    for (u = 0; u < uDivision; u++) {
+        var aIndex = rowAIndex + u;
+        var bIndex = rowBIndex + u;
+
+      all =  Triangle.constructTriangle(
+            rowB[bIndex],
+            rowA[aIndex + 1],
+            rowB[bIndex + 1],
+             all, u, idx);
+    }
+}
+
+console.log("ALL-----------------------------------------------------");
+all.forEach(numb => {
+console.log(numb);
+});
+
+ var geometryTri = new THREE.BufferGeometry();
+
+ geometryTri.addAttribute( 'position', new THREE.BufferAttribute( all, 3 ) );
+ geometryTri.addAttribute( 'normal', new THREE.BufferAttribute( all, 3 ) );
+
+ var meshTri = new THREE.Mesh( geometryTri, material2 );
+
+  scene.add(meshTri);
+
+// //-------------------------------------------
+// //Triangle Down A
+// //-------------------------------------------
+
+// //Anzahl der Reihen
+// for (int v = 0; v < countVOdd; v++)
+// {
+//   int rowAIndex = v * aCount;
+//   int rowBIndex = v * bCount;
+
+//   int idx = ConstructIdx(v, triangleEachRow, 0);
+
+//   for (int u = 0; u < bCount; u++)
+//   {
+//     int aIndex = rowAIndex + u;
+//     int bIndex = rowBIndex + u;
+
+//     ConstructTriangle(
+//       rowA[aIndex],
+//       rowA[aIndex + 1],
+//       rowB[bIndex],
+//       ref all, u, idx);
+//   }
+// }
+
+// //-------------------------------------------
+// //Triangle Down B
+// //-------------------------------------------
+
+// //Anzahl der Reihen
+// for (int v = 0; v < countVEven - 1; v++)
+// {
+//   int rowAIndex = (v + 1) * aCount;
+//   int rowBIndex = v * bCount;
+
+//   int idx = ConstructIdx(v, triangleEachRow, triangleEachRow + 1);
+
+//   for (int u = 0; u < uDivision; u++)
+//   {
+//     int aIndex = rowAIndex + u;
+//     int bIndex = rowBIndex + u;
+
+//     ConstructTriangle(
+//       rowB[bIndex],
+//       rowA[aIndex + 1],
+//       rowB[bIndex + 1],
+//       ref all, u, idx);
+//   }
+// }
+
+// //-------------------------------------------
+// //Triangle Up B
+// //-------------------------------------------
+
+// //Anzahl der Reihen
+// for (int v = 0; v < countVEven - 1; v++)
+// {
+//   int rowAIndex = (v + 1) * aCount;
+//   int rowBIndex = v * bCount;
+
+//   int idx = ConstructIdx(v, triangleEachRow, triangleEachRow);
+
+//   for (int u = 0; u < bCount; u++)
+//   {
+//     int aIndex = rowAIndex + u;
+//     int bIndex = rowBIndex + u;
+
+//     ConstructTriangle(
+//       rowA[aIndex],
+//       rowA[aIndex + 1],
+//       rowB[bIndex],
+//       ref all, u, idx);
+//   }
+// }
 
 
 renderer.render(scene, camera);
@@ -273,7 +438,8 @@ renderer.render(scene, camera);
 var render = function () {
     requestAnimationFrame(render);
 
-    //  object.rotation.y += 0.005;
+    // geometryBla.rotation.y += 0.005;
+  //  meshSquare.rotation.x += 0.005;
 
     // Render the scene
     renderer.render(scene, camera);
--- js/triangle.js
+++ js/triangle.js
@@ -2,36 +2,107 @@
  * @author leobroom
  **/
 
-Triangle = {  
-
-//     createRows: function(cp1 , cp2, rows) 
-//     {
-//        var count = cp1.length;
-//        var lists =  new Array(rows+1);      
-     
-//        for (u = 0; u < rows+1; u++) 
-//        { 
-//            var cps =  new Array(count);
-
-//            for ( i = 0; i < count; i++)
-//            {
-//                var vA = cp1[i];
-//                var vB = cp2[i];
-       
-//                var vC = new THREE.Vector3();
- 
-//                vC = vC.subVectors(vB, vA ) ;
-//                vC.multiplyScalar (u/ rows);        
-//                vC.add(vA);
-       
-//                cps[i] = vC; 
-//            }
-
-//            lists[u] = new THREE.NURBSCurve( nurbsDegree, nurbsKnots, cps );
-//        }
+Triangle = {
 
 
-//        return lists;
-//    },
+    constructTriangle: function (a, b, c, all, u, idx) {
+        // *3 wegen des BufferArrays
+
+        var aBuf = Triangle.deconstructVec(a);
+        var bBuf = Triangle.deconstructVec(b);
+        var cBuf = Triangle.deconstructVec(c);
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i) + u * 2] = aBuf[i];
+        }
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i+3) + u * 2] = bBuf[i];
+        }
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i+6) + u * 2] = cBuf[i];
+        }
+        // console.log("a:" +aBuf)
+        // console.log("b:" +bBuf)
+        // console.log("c:" +cBuf)
+
+        return all;
+    },
+
+    
+    constructTriangle: function (a, b, c, all, u, idx) {
+        // *3 wegen des BufferArrays
+
+        var aBuf = Triangle.deconstructVec(a);
+        var bBuf = Triangle.deconstructVec(b);
+        var cBuf = Triangle.deconstructVec(c);
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i) + u * 2] = aBuf[i];
+        }
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i+3) + u * 2] = bBuf[i];
+        }
+
+        for (i = 0; i < 3; i++) {
+            all[(idx + i+6) + u * 2] = cBuf[i];
+        }
+        // console.log("a:" +aBuf)
+        // console.log("b:" +bBuf)
+        // console.log("c:" +cBuf)
+
+        return all;
+    },
+
+
+    deconstructVec: function (vec) {
+        // *3 wegen des BufferArrays
+
+        var bufVec = new Float32Array(3);
+
+        bufVec[0] = vec.x;
+        bufVec[1] = vec.y;
+        bufVec[2] = vec.z;
+
+        return bufVec;
+    },
+
+
+    constructIdx: function (v, triangleEachRow, modificator) {
+        return modificator + (triangleEachRow * v * 2);
+    }
+
+
+    //     createRows: function(cp1 , cp2, rows) 
+    //     {
+    //        var count = cp1.length;
+    //        var lists =  new Array(rows+1);      
+
+    //        for (u = 0; u < rows+1; u++) 
+    //        { 
+    //            var cps =  new Array(count);
+
+    //            for ( i = 0; i < count; i++)
+    //            {
+    //                var vA = cp1[i];
+    //                var vB = cp2[i];
+
+    //                var vC = new THREE.Vector3();
+
+    //                vC = vC.subVectors(vB, vA ) ;
+    //                vC.multiplyScalar (u/ rows);        
+    //                vC.add(vA);
+
+    //                cps[i] = vC; 
+    //            }
+
+    //            lists[u] = new THREE.NURBSCurve( nurbsDegree, nurbsKnots, cps );
+    //        }
+
+
+    //        return lists;
+    //    },
 
 }
